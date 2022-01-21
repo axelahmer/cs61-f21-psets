67738f260f40334bfb48cd91034828df27036f59
Axel Ahmer <axel.thor.ahmer@gmail.com>
2022-01-21 23:22:17 +1100

    tests 1-17 passing

--- pset1/m61.cc
+++ pset1/m61.cc
@@ -5,6 +5,9 @@
 #include <cstdio>
 #include <cinttypes>
 #include <cassert>
+#include <iostream>
+
+static m61_statistics gstats = {0, 0, 0, 0, 0, 0, 0, 0};
 
 /// m61_malloc(sz, file, line)
 ///    Return a pointer to `sz` bytes of newly-allocated dynamic memory.
@@ -14,8 +17,43 @@
 
 void* m61_malloc(size_t sz, const char* file, long line) {
     (void) file, (void) line;   // avoid uninitialized variable warnings
-    // Your code here.
-    return base_malloc(sz);
+
+    // call malloc
+    void* ret_ptr = base_malloc(sz);
+
+    // check if first allocation and set heap min
+    if(gstats.heap_min == 0) gstats.heap_min = (uintptr_t)ret_ptr;
+
+    
+    // std::cout << (int*)ret_ptr << std::endl;
+    if (ret_ptr) // if not null pointer
+    {
+        ++gstats.ntotal;
+        ++gstats.nactive;
+        gstats.total_size+=sz;
+        gstats.active_size+=sz;
+
+        // update min and max heap values
+        if((uintptr_t)ret_ptr < gstats.heap_min) gstats.heap_min = (uintptr_t)ret_ptr;
+        if((uintptr_t)ret_ptr + sz > gstats.heap_max) gstats.heap_max = (uintptr_t)ret_ptr + sz;
+    }
+    else
+    {
+        gstats.nfail++;
+        gstats.fail_size+=sz;
+    }
+
+
+    // if ((uintptr_t)ret_ptr < gstats.heap_min)
+    // {
+    //     gstats.heap_min = ret_ptr;
+    // }
+    // if ((uintptr_t)ret_ptr > gstats.heap_max)
+    // {
+    //     gstats.heap_max = ret_ptr;
+    // }
+
+    return ret_ptr;
 }
 
 
@@ -26,7 +64,11 @@ void* m61_malloc(size_t sz, const char* file, long line) {
 
 void m61_free(void* ptr, const char* file, long line) {
     (void) file, (void) line;   // avoid uninitialized variable warnings
-    // Your code here.
+    if (ptr)
+    {
+        --gstats.nactive;
+    }
+    
     base_free(ptr);
 }
 
@@ -53,8 +95,7 @@ void* m61_calloc(size_t nmemb, size_t sz, const char* file, long line) {
 
 void m61_get_statistics(m61_statistics* stats) {
     // Stub: set all statistics to enormous numbers
-    memset(stats, 255, sizeof(m61_statistics));
-    // Your code here.
+    memcpy(stats, &gstats, sizeof(gstats));
 }
 
 
